59874b8158772e6859d87efeb04bd3b6e6e74f6e
Mavia <mauwia.atif@gmail,com>
2019-09-29 13:11:00 +0500

    Updates

---
+++ precache-manifest.72c44ee4d9ac114351055a93f2c48ea3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e8103d8ab8c9e3cc103b8bb6f81b6d",
+    "url": "/Youtube-API-React/index.html"
+  },
+  {
+    "revision": "cc8a20b803f9291c0330",
+    "url": "/Youtube-API-React/static/css/main.5f2f3666.chunk.css"
+  },
+  {
+    "revision": "5890776a2bd5034de5b3",
+    "url": "/Youtube-API-React/static/js/2.d11d7ef0.chunk.js"
+  },
+  {
+    "revision": "cc8a20b803f9291c0330",
+    "url": "/Youtube-API-React/static/js/main.36510c67.chunk.js"
+  },
+  {
+    "revision": "b7e1cb8dfd30675570fb",
+    "url": "/Youtube-API-React/static/js/runtime-main.a789d0fd.js"
+  }
+]);
